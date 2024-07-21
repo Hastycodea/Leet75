99bcea74f610d89d33d4f652f808a48392c3b692
hastycodea <hastycodeandreh@gmail.com>
2024-07-21 15:24:24 +0300

    vowels

--- src/main/java/ReverseVowels.java
+++ src/main/java/ReverseVowels.java
@@ -1,2 +1,36 @@
 public class ReverseVowels {
+    public static void main(String[] args) {
+        System.out.println(reverseVowels("Hellohayabusa"));
+        
+    }
+    public static String reverseVowels(String word) {
+        char[] wordArray = word.toCharArray();
+        
+        int start = 0;
+        int end = word.length() - 1;
+
+        String vowels = "aeiouAEIOU";
+
+        while(start < end) {
+            while (start < end && vowels.indexOf(wordArray[start]) == -1) {
+                start++;
+            }
+            while (start < end && vowels.indexOf(wordArray[end]) == -1) {
+                end--;
+            }
+
+            //swapping
+            char temp = wordArray[start];
+            wordArray[start] = wordArray[end];
+            wordArray[end] = temp;
+
+            start++;
+            end--;
+
+        }
+
+        String result = new String(wordArray);
+
+        return result;
+    }
 }
