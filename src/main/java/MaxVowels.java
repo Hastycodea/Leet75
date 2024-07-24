945cf5d110ac297cb8f44f8cacc5eec23919335b
Hastycode Andreh <hastycodeandreh@gmail.com>
2024-07-24 10:11:17 +0300

    MaxVowels

---
+++ src/main/java/MaxVowels.java
@@ -0,0 +1,34 @@
+public class MaxVowels {
+    public static void main(String[] args) {
+
+        int result = maxVowels("abciiidef", 3);
+
+        System.out.println(result);
+        
+    }
+    public static int maxVowels(String s, int k) {
+
+        int window = 0;
+        int result = 0;
+
+        for(int i = 0; i < k; i++) {
+            if(isVowel(s.charAt(i))) window++;
+        }
+
+        result = window;
+
+        for(int j = k; j < s.length(); j++) {
+            if(isVowel(s.charAt(j))) window++;
+
+            if(isVowel(s.charAt(j - k))) window--;
+
+            result = Math.max(result, window);
+        }
+
+        return result;
+    }
+
+    public static boolean isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
+}
