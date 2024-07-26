990253a45b425d874b7432eff33c3f46635b7621
Hastycode Andreh <hastycodeandreh@gmail.com>
2024-07-26 18:44:25 +0300

    to review

---
+++ src/main/java/LongestSubArray.java
@@ -0,0 +1,38 @@
+public class LongestSubArray {
+
+    public static void main(String[] args) {
+        int[] nums = {0,1,1,1,0,1,1,0,1};
+        System.out.println(longestSub(nums));        
+    }
+
+    public static int longestSub(int[] nums) {
+        
+        int i = 0, j=0, max=0, pos=0;
+
+        while(pos < nums.length) {
+            if(nums[pos] == 1) {
+                pos++;
+                i++;
+            } else {
+                pos++;
+                j++;
+
+
+                while (pos < nums.length && nums[pos] == 1) {
+                    pos++;
+                    j++;
+                }
+
+                max = Math.max(max, j + i);
+                i = j;
+            }
+        }
+
+        if (i == nums.length) {
+            max = nums.length - 1; 
+            
+        }
+        return max;
+    }
+    
+}
