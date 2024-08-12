a7b21ab9684ca01392caea6db9713caa2e0d9c73
Hastycode Andreh <hastycodeandreh@gmail.com>
2024-08-12 15:23:59 +0300

    RemoveStars

---
+++ src/main/java/RemoveStars.java
@@ -0,0 +1,33 @@
+import java.util.Stack;
+
+public class RemoveStars {
+    public static void main(String[] args) {
+        String name = "lee**tco*de*";
+        System.out.println(removeStars(name));
+        
+    }
+
+    public static String removeStars(String word) {
+
+        Stack<Character> wordStack = new Stack<>();
+        
+        for(int i = 0; i < word.length(); i++) {
+            if(word.charAt(i) == '*') {
+                if(!wordStack.isEmpty()) {
+                    wordStack.pop();
+                } 
+            } else {
+                wordStack.add(word.charAt(i));
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        while(!wordStack.isEmpty()) {
+            sb.append(wordStack.pop());
+        }
+
+        return sb.reverse().toString();
+
+    }
+}
